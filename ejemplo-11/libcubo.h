c9f256e8d2dac96c0d9e5203aa183c484808f93f
Juan-Camilo-Suarez137 <juan.suarez04@uceva.edu.co>
2025-04-05 02:27:36 +0000

    sesion de 04-04-25 por problemas de conexion  a esta hora y la sesion 01-04-25  se termino de hacer porque saltaba algunos errores

---
+++ ejemplo-11/libcubo.h
@@ -0,0 +1,17 @@
+//libcubo.h
+#ifndef LIBCUBO_H
+#define LIBCUBO_H
+
+// Signature de las funciones
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  double calcularCubo(double numero);
+  int multiplicarPorOcho(int numero);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
